82a0e7ac9ce109aaa62fad7198bafa7f36615c23
Luwangel <adrien.amoros@gmail.com>
2019-01-04 14:41:11 +0100

    Design Menu

--- packages/app/src/layout/Menu.vue
+++ packages/app/src/layout/Menu.vue
@@ -1,26 +1,53 @@
 <template>
-  <nav>
-    <p>Title</p>
-  </nav>
+  <header>
+    <div class="logo">
+      <img alt="Vue logo" src="../assets/logo.png">
+      <span>{{title}}</span>
+    </div>
+  </header>
 </template>
 
 <script>
 export default {
   name: "menu",
   props: {
-    title: String
+    title: { type: String, default: "Play the 15 Puzzle Game" }
   }
 };
 </script>
 
 <style scoped>
-nav {
+header {
   background-color: #fff;
+  box-shadow: 0 0 1px rgba(0, 0, 0, 0.25);
   position: fixed;
   top: 0;
-  height: 40px;
-  width: 100%;
-  padding: 10px 60px;
+  height: 50px;
+  width: calc(100%);
   z-index: 100;
+}
+
+header > div {
+  padding: 5px 60px;
+}
+
+.logo {
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+}
+
+.logo > img {
+  margin-right: 6px;
+  width: 40px;
+  height: 40px;
+}
+
+.logo > span {
+  font-size: 1.5em;
+  line-height: 40px;
+  color: #2c3e50;
+  font-family: "Dosis", "Source Sans Pro", "Helvetica Neue", Arial, sans-serif;
+  font-weight: 500;
 }
 </style>
