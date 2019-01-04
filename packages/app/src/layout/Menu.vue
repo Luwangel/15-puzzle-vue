acae258e7e93f96d3351686c1e18038805286816
Luwangel <adrien.amoros@gmail.com>
2019-01-04 14:10:32 +0100

    Prettier format

---
+++ packages/app/src/layout/Menu.vue
@@ -0,0 +1,26 @@
+<template>
+  <nav>
+    <p>Title</p>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Menu",
+  props: {
+    title: String
+  }
+};
+</script>
+
+<style>
+nav {
+  background-color: #fff;
+  position: fixed;
+  top: 0;
+  height: 40px;
+  width: 100%;
+  padding: 10px 60px;
+  z-index: 100;
+}
+</style>
