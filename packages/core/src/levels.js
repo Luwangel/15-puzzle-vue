f73ba3223d3faae98400f2854d41372cc9572bee
Luwangel <adrien.amoros@gmail.com>
2019-01-13 18:00:31 +0100

    Refact the levels

--- packages/core/src/levels.js
+++ packages/core/src/levels.js
@@ -1,13 +1,11 @@
-export const GENIUS = 0;
 export const GOLD = 1;
 export const SILVER = 2;
 export const BRONZE = 3;
 export const NOT_RANKED = 4;
 
-export const byMoves = moves => {
-    if (moves <= 0) return 0;
-    if (moves < 80) return 1;
-    if (moves < 120) return 2;
-    if (moves < 180) return 3;
-    return 4;
+export const levelByMoves = moves => {
+    if (moves < 80) return GOLD;
+    if (moves < 120) return SILVER;
+    if (moves < 180) return BRONZE;
+    return NOT_RANKED;
 };
