da1bdaa88868b8738b4ff138cc3cd6b92e0905fe
Luwangel <adrien.amoros@gmail.com>
2019-01-15 20:50:06 +0100

    Create a Button component

---
+++ packages/app/src/components/Button.vue
@@ -0,0 +1,33 @@
+<template>
+  <button @click="$emit('click')" @keyup.enter="$emit('click')">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    content: String
+  }
+};
+</script>
+
+<style scoped>
+button {
+  cursor: pointer;
+  border-radius: 6px;
+  border: 1px solid;
+  border-color: #42b983;
+  background-color: #fff;
+  color: #42b983;
+  transition: all 0.15s ease;
+  padding: 0.75em 2em;
+  outline: none;
+}
+
+button:hover {
+  color: #1c7950;
+  border-color: #1c7950;
+}
+</style>
