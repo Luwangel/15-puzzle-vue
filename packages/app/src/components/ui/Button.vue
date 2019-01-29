1daabc8298b6affd77eb40261c377ffa31b195f2
Luwangel <adrien.amoros@gmail.com>
2019-01-29 21:42:21 +0100

    Add a disabled option to the Button

--- packages/app/src/components/ui/Button.vue
+++ packages/app/src/components/ui/Button.vue
@@ -1,23 +1,34 @@
 <template>
-  <button @click="$emit('click')" @keyup.enter="$emit('click')">
+  <button @click="handleClick()" @keyup.enter="handleClick()" :class="{ disabled: disabled }">
     <slot></slot>
   </button>
 </template>
 
 <script>
 export default {
-  name: "Button"
+  name: "Button",
+  props: {
+    disabled: { type: Boolean, default: false }
+  },
+  methods: {
+    handleClick() {
+      if (this.disabled) {
+        return;
+      }
+      this.$emit("click");
+    }
+  }
 };
 </script>
 
 <style scoped>
 button {
   cursor: pointer;
-  border-radius: 6px;
-  border: 1px solid;
-  border-color: #42b983;
-  background-color: #fff;
   color: #42b983;
+  background-color: #fff;
+  border: 1px solid;
+  border-radius: 6px;
+  border-color: #42b983;
   transition: all 0.15s ease;
   padding: 0.75em 2em;
   outline: none;
@@ -26,5 +37,12 @@ button {
 button:hover {
   color: #1c7950;
   border-color: #1c7950;
+}
+
+button.disabled,
+button.disabled:hover {
+  cursor: default;
+  color: #7f8c8d;
+  border-color: #7f8c8d;
 }
 </style>
