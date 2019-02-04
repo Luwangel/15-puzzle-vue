f4ec51e12e3f7781887a9f65dd010528b6580b96
Luwangel <adrien.amoros@gmail.com>
2019-02-04 21:29:21 +0100

    Improve loading size

--- packages/app/src/components/Loading.vue
+++ packages/app/src/components/Loading.vue
@@ -5,14 +5,14 @@ export default {
 </script>
 
 <template>
-  <div>
-    <img class="loading" alt="Loading..." src="../assets/images/loading.gif">
-  </div>
+  <img class="loading" alt="Loading..." src="../assets/images/loading.gif">
 </template>
 
 <style scoped>
 .loading {
-  height: 500px;
-  width: 500px;
+  height: 75%;
+  min-height: 200px;
+  width: 75%;
+  min-width: 200px;
 }
 </style>
