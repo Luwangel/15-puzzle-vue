40489d1ae0aee14852bdc9ee1e2fe003c1152366
Luwangel <adrien.amoros@gmail.com>
2019-02-04 21:46:15 +0100

    Compile the Title with webpack

--- packages/app/vue.config.js
+++ packages/app/vue.config.js
@@ -1,3 +1,11 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production' ? '/15-puzzle-vue/' : '/',
+    pages: {
+        index: {
+            entry: 'src/main.js',
+            template: 'public/index.html',
+            filename: 'index.html',
+            title: 'Play the 15 Puzzle Game',
+        },
+    },
 };
