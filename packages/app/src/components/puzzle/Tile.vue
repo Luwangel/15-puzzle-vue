4b1ed8e9af0264bb48a5345de9ea44d147933313
Luwangel <adrien.amoros@gmail.com>
2019-02-07 16:09:36 +0100

    Add a transition to display Tile numbers

--- packages/app/src/components/puzzle/Tile.vue
+++ packages/app/src/components/puzzle/Tile.vue
@@ -26,7 +26,9 @@ export default {
 
 <template>
   <div :class="classes" :style="[backgroundStyle, dimensionStyle, { backgroundImage: image }]">
-    <span v-if="showNumber" class="puzzle-tile-number">{{number}}</span>
+    <transition name="fade">
+      <span v-if="showNumber" class="puzzle-tile-number">{{number}}</span>
+    </transition>
   </div>
 </template>
 
@@ -52,5 +54,15 @@ export default {
   font-size: 1.5em;
   color: #fff;
   text-align: center;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.2s;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
