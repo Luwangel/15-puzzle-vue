f3b35b6975a97ecd316bdccf7920b770a1f04644
Luwangel <adrien.amoros@gmail.com>
2019-02-18 21:44:20 +0100

    Create a link component

---
+++ packages/app/src/components/ui/Link.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+  name: "Link",
+  props: {
+    href: { type: String, default: "" }
+  }
+};
+</script>
+
+<template>
+  <a :href="href">
+    <slot/>
+  </a>
+</template>
+
+<style scoped>
+a {
+  cursor: pointer;
+  color: #424fb9;
+  transition: all 0.15s ease;
+  padding: 0.5em 1em;
+  outline: none;
+  text-decoration: none;
+}
+
+a:hover {
+  color: #162473;
+  border-color: #162473;
+  text-decoration: underline;
+}
+</style>
